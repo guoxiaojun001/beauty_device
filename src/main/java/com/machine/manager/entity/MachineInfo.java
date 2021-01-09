ee125ef79b4a223b921b99807f82cd51f0a33c0e
guoxi <497350165@qq.com>
2021-01-09 23:40:55 +0800

    修改表字段

--- src/main/java/com/machine/manager/entity/MachineInfo.java
+++ src/main/java/com/machine/manager/entity/MachineInfo.java
@@ -1,6 +1,7 @@
 package com.machine.manager.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import lombok.Data;
 
 /**
@@ -20,6 +21,28 @@ public class MachineInfo implements Serializable {
     private String machineAttribute;
 
     private Integer userId;
+
+    /**
+     * 使用时长
+     */
+    private Integer usedDuration;
+
+    /**
+     * 品牌
+     */
+    private String machineBrand;
+
+    private String userName;
+
+    /**
+     * 设备状态
+     */
+    private String machineStatus;
+
+    /**
+     * 单词使用时长
+     */
+    private Date machineWorkTimeOnce;
 
     private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/machine/manager/entity/UserInfo.java
+++ src/main/java/com/machine/manager/entity/UserInfo.java
@@ -1,37 +1,29 @@
 package com.machine.manager.entity;
 
 import java.io.Serializable;
-
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * user_info
- *
- * @author
+ * @author 
  */
 @Data
 public class UserInfo implements Serializable {
-    @ApiModelProperty(value = "试卷ID",example = "1")
     private Integer id;
 
-    @ApiModelProperty
     private String name;
 
-    @ApiModelProperty
     private String email;
 
-    @ApiModelProperty
     private String telephone;
 
-    @ApiModelProperty
     private String address;
 
-    @ApiModelProperty
     private String password;
 
-    @ApiModelProperty
     private String roleId;
+
+    private String userType;
 
     private static final long serialVersionUID = 1L;
 }
